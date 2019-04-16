66ce685d44d520786e2e80142c9184fae1fed6a4
Guilherme <gsantosi@indracompany.com>
2019-04-16 08:54:43 -0300

    Classe Departamento Criada

---
+++ src/Testes/TesteDep.java
@@ -0,0 +1,15 @@
+package Testes;
+
+import Aplicacao.Departamento;
+
+public class TesteDep {
+	public static void main(String[] args) {
+
+		Departamento dep = new Departamento(1,"Guilherme");
+		
+		System.out.println(dep);
+		
+		
+		
+	}
+}
---
+++ src/Testes/TesteVend.java
@@ -0,0 +1,22 @@
+package Testes;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import Aplicacao.Departamento;
+import Aplicacao.Vendedor;
+
+public class TesteVend {
+   public static void main(String [] args) {
+	   
+	   SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	   
+	   Departamento dep = new Departamento(7,"Perecíveis");
+	   
+	   Vendedor vend = new Vendedor(2,"Raimundo","Raimundo15@gmail.com",new Date(1),9000, dep);
+	   
+	   System.out.println(vend);
+	   
+	   
+   }
+}
